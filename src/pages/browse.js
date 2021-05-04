c0d28341c136631e335d1e0b5d0d87caf0498446
Dmytro Murashko <riddon04@gmail.com>
2021-05-04 11:59:51 +0300

    Got firebase content and creaeted custom util for it.

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {useContent} from '../hooks';
+import selectionFilter from '../utils/selection-filter';
+import { BrowseContainer } from '../containers/browse';
 
-export default function SignIn() {
+export default function Browse() {
+
+    const { series } = useContent('series');
+    const { films } = useContent('films');
+
+    const slides = selectionFilter({series, films});
+    console.log(slides);
+
     return (
-        <p>
-            It`s a Browse, Ser)
-        </p>
+        <BrowseContainer slides={slides}/>
     );
 }
